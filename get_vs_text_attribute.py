fe8a2fcc2c86526157f832e49dd1972b53bc590b
Biswanth <pinnikabiswanth@gmail.com>
2026-02-04 13:46:27 +0530

    textgetattributes

---
+++ get_vs_text_attribute.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+driver.get("https://demo.nopcommerce.com/")
+driver.maximize_window()
+
+#text attribute will return what is on the web page
+# when we use get it will written what we write in the value or search box
+
+
+
+
+
+
+
+
